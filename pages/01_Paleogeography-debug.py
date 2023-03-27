a9d50dde34ffb735dda29551bd0ccc3aa9acae40
sebsteinig <seb.steinig@gmail.com>
2023-03-27 20:16:36 +0100

    test

--- pages/01_Paleogeography-debug.py
+++ pages/01_Paleogeography-debug.py
@@ -23,13 +23,9 @@ for v in [modern_lat, modern_lon, user_user_variable]:
 df_locations = get_paleo_location_herold(modern_lat, modern_lon)
 st.dataframe(df_locations.style.format("{:.1f}"))
 
-# fig_geo = plot_paleogeography(df_locations, "global")
+fig_geo = plot_paleogeography(df_locations, "global")
 
-# fig, ax = plt.subplots(subplot_kw=dict(projection=ccrs.Robinson()))
-
-# rotationFile.LAT.plot(ax=ax, transform=ccrs.PlateCarree())
-
-# st.pyplot(fig_geo)
+st.pyplot(fig_geo)
 
 # fn = 'scatter.png'
 # img = io.BytesIO()
